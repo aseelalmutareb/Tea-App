ad5a644706a60466618542361b6bd77be6512f50
Aseel <aseel.almutareb@gmail.com>
2022-07-07 15:48:29 +0200

    replace dataset with db

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,13 +1,11 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 
-// import users data
-const users = require("../datasets/userData");
+const User = require("../models/User");
 const generateToken = require("../helpers/generateToken");
 
 const register = async (req, res) => {
   // validation, sanitization, authorization
-
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({
@@ -18,26 +16,27 @@ const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   // find a user with same email, if found send error: email is taken
+  const isRegistered = await User.findOne({ email });
+  if(isRegistered) return res.status(409).json(`${email} is already registered`);
 
-  const Id = users.length;
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = {
-    id: Id,
-    name: name,
-    email: email,
-    password: hashedPassword,
-  };
-
-  users.push(user);
+  const user = await User.create(
+    {
+      name,
+      email,
+      password: hashedPassword
+    }
+  );
 
   // authorization with jwt
   // prepare a payload
   const payload = {
-      id: user.id,
+      id: user._id,
       name: user.name,
   };
+
   // sign and generate the token
   try {
     const token = await generateToken(payload);
@@ -51,7 +50,8 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     const { email, password } = req.body;
     // find user
-    const user = users.find(user => user.email == email);
+    const user = await User.findOne({email});
+
     if(!user) return res.status(404).json({ message: 'Either email or password is incorrect!'});
     
     try {
